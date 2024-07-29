d68846d04413b883fe9de9fb821fd3e0f62d37c8
ChhimiDorji <chimtshe75@gmail.com>
2024-07-29 16:14:07 +1000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+﻿<?php
+function findSingleNumber($nums) {
+    $singleNumber = 0;
+    
+    // XOR all elements in the array
+    foreach ($nums as $num) {
+        $singleNumber ^= $num;
+    }
+    
+    return $singleNumber;
+}
+
+// Example usage:
+$nums = array(5, 3, 4, 3, 4);
+$single = findSingleNumber($nums);
+echo "The single number in the array is: $single\n";
+?>
